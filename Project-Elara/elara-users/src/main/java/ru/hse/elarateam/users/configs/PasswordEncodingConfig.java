db61fb9ad2f399e1cb5b81c04ad5424e0c99d709
germandilio <glmikhaylov@edu.hse.ru>
2023-04-08 15:35:52 +0300

    Use fixed salt

--- Project-Elara/elara-users/src/main/java/ru/hse/elarateam/users/configs/PasswordEncodingConfig.java
+++ Project-Elara/elara-users/src/main/java/ru/hse/elarateam/users/configs/PasswordEncodingConfig.java
@@ -1,14 +1,22 @@
 package ru.hse.elarateam.users.configs;
 
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.security.SecureRandom;
+
+@Setter
 @Configuration
 public class PasswordEncodingConfig {
+    @Value("${elara.users.password-encoding.salt}")
+    private String salt;
+
     @Bean
     public PasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(12, new SecureRandom(salt.getBytes()));
     }
 }
