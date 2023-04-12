09304978638623b45587e68f80bcff6f5e653248
germandilio <glmikhaylov@edu.hse.ru>
2023-04-13 00:26:43 +0300

    Fix price range request, add filters and query params

--- Project-Elara/elara-products-get/src/main/java/ru/hse/elarateam/productsget/web/repository/ProductsRepository.java
+++ Project-Elara/elara-products-get/src/main/java/ru/hse/elarateam/productsget/web/repository/ProductsRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestParam;
+import ru.hse.elarateam.productsget.dto.PriceRangeDTO;
 import ru.hse.elarateam.productsget.model.Product;
 
 import java.math.BigDecimal;
@@ -21,39 +21,83 @@ import java.util.UUID;
 public interface ProductsRepository extends JpaRepository<Product, UUID> {
     @NonNull
     @Override
-    Optional<Product> findById(@NonNull @RequestParam("id") UUID uuid);
+    Optional<Product> findById(@NonNull UUID uuid);
 
     @NonNull
     @Override
     Page<Product> findAll(@NonNull Pageable pageable);
 
     @Operation(description = "Find products by ids")
-    Page<Product> findAllByIdIn(@RequestParam("ids") Collection<UUID> uuids, Pageable pageable);
+    Page<Product> findAllByIdIn(Collection<UUID> uuids, Pageable pageable);
 
-    Page<Product> findAllByUpcIn(@RequestParam("upcs") Collection<String> upcs, Pageable pageable);
+    Page<Product> findAllByUpcIn(Collection<String> upcs, Pageable pageable);
 
-    Page<Product> findAllByNameContainingIgnoreCase(@RequestParam("name") String name, Pageable pageable);
+    Page<Product> findAllByNameContainingIgnoreCase(String name, Pageable pageable);
 
     Page<Product> findAllByOrderByCreatedDateDesc(Pageable pageable);
 
-    @Query("SELECT MIN(p.price) FROM Product p")
-    BigDecimal findMinPrice();
+    @Query(value = """
+                SELECT MIN(p.price) AS min, MAX(p.price) AS max FROM products p
+                LEFT JOIN products_colors pc ON pc.product_id = p.id
+                LEFT JOIN colors c ON pc.colors_id = c.id
+                JOIN products_features pf on p.id = pf.product_id
+                JOIN features f on f.id = pf.features_id
+                JOIN products_sports ps on p.id = ps.product_id
+                JOIN sports s on ps.sports_id = s.id
+                 WHERE (:sports IS NULL OR s.name IN :sports) AND
+                        (:colors IS NULL OR c.name IN :colors) AND
+                         (:features IS NULL OR f.name IN :features) AND
+                          (:countries IS NULL OR p.country_of_origin IN :countries) AND
+                           (:brands IS NULL OR p.brand IN :brands) AND
+                            (:sizeUS IS NULL OR p.sizeus IN :sizeUS) AND
+                             (:sizeUK IS NULL OR p.sizeuk IN :sizeUK) AND
+                              (:sizeEUR IS NULL OR p.sizeeur IN :sizeEUR) AND
+                               (:query IS NULL OR p.name LIKE CONCAT('%', :query, '%') OR c.name LIKE CONCAT('%', :query, '%'))
+            """, nativeQuery = true)
+    PriceRangeDTO findPriceRange(Collection<String> sports,
+                                 Collection<String> colors,
+                                 Collection<String> features,
+                                 Collection<String> countries,
+                                 Collection<String> brands,
+                                 Collection<Double> sizeUS,
+                                 Collection<Double> sizeEUR,
+                                 Collection<Double> sizeUK,
+                                 String query);
 
-    @Query("SELECT MAX(p.price) FROM Product p")
-    BigDecimal findMaxPrice();
+    default PriceRangeDTO findPriceRangeNullable(Collection<String> sports,
+                                                 Collection<String> colors,
+                                                 Collection<String> features,
+                                                 Collection<String> countries,
+                                                 Collection<String> brands,
+                                                 Collection<Double> sizeUS,
+                                                 Collection<Double> sizeEUR,
+                                                 Collection<Double> sizeUK,
+                                                 String query) {
+        // verification part
+        Collection<String> prSports = sports == null ? Collections.emptyList() : sports;
+        Collection<String> prColors = colors == null ? Collections.emptyList() : colors;
+        Collection<String> prFeatures = features == null ? Collections.emptyList() : features;
+        Collection<String> prCountries = countries == null ? Collections.emptyList() : countries;
+        Collection<String> prBrands = brands == null ? Collections.emptyList() : brands;
+        Collection<Double> prSizeUS = sizeUS == null ? Collections.emptyList() : sizeUS;
+        Collection<Double> prSizeEUR = sizeEUR == null ? Collections.emptyList() : sizeEUR;
+        Collection<Double> prSizeUK = sizeUK == null ? Collections.emptyList() : sizeUK;
+        String prQuery = query == null ? "" : query;
+        return findPriceRange(prSports, prColors, prFeatures, prCountries, prBrands, prSizeUS, prSizeEUR, prSizeUK, prQuery);
+    }
 
     Page<Product> findAllBySports_NameInAndColors_NameInAndFeatures_NameInAndCountryOfOriginInAndBrandInAndSizeUSInAndSizeEURInAndSizeUKInAndPriceBetweenAndNameContainingIgnoreCase(
-            @RequestParam("sports") Collection<String> sports,
-            @RequestParam("colors") Collection<String> colors,
-            @RequestParam("features") Collection<String> features,
-            @RequestParam("countries") Collection<String> countries,
-            @RequestParam("brands") Collection<String> brands,
-            @RequestParam("sizeUS") Collection<Double> sizeUS,
-            @RequestParam("sizeEUR") Collection<Double> sizeEUR,
-            @RequestParam("sizeUK") Collection<Double> sizeUK,
-            @RequestParam("minPrice") @NotNull BigDecimal minPrice,
-            @RequestParam("maxPrice") @NotNull BigDecimal maxPrice,
-            @RequestParam("query") String query,
+            Collection<String> sports,
+            Collection<String> colors,
+            Collection<String> features,
+            Collection<String> countries,
+            Collection<String> brands,
+            Collection<Double> sizeUS,
+            Collection<Double> sizeEUR,
+            Collection<Double> sizeUK,
+            @NotNull BigDecimal minPrice,
+            @NotNull BigDecimal maxPrice,
+            String query,
             Pageable pageable);
 
     default Page<Product> findAllByFiltersAndQueryNullable(
