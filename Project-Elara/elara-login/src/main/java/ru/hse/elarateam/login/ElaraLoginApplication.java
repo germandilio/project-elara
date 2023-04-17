e06f9e3002ef6e015abab2d98faa8c5f82a834c9
lemon-doge <ddtatyanchenko@edu.hse.ru>
2023-04-17 23:50:20 +0300

    adjusted login for local testing

--- Project-Elara/elara-login/src/main/java/ru/hse/elarateam/login/ElaraLoginApplication.java
+++ Project-Elara/elara-login/src/main/java/ru/hse/elarateam/login/ElaraLoginApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import ru.hse.elarateam.login.model.Role;
+import ru.hse.elarateam.login.model.RoleEnum;
+import ru.hse.elarateam.login.model.UserServiceInfo;
+import ru.hse.elarateam.login.web.repositories.UsersRepository;
 import ru.hse.elarateam.login.web.services.jwt.JWTUtilsImpl;
 
 import java.math.BigDecimal;
@@ -16,11 +21,13 @@ public class ElaraLoginApplication {
         SpringApplication.run(ElaraLoginApplication.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner initDatabase(JWTUtilsImpl JWTUtils) {
-//        return args -> {
-//            var token = JWTUtils.generateToken("lemon-doge-admin-login");
+    @Bean
+    @Transactional
+    public CommandLineRunner initDatabase(JWTUtilsImpl JWTUtils) {
+        return args -> {
+//            var token = JWTUtils.generateToken("admin@gmail.com");
 //            log.info("admin token: " + token);
-//        };
-//    }
+            log.info(JWTUtils.getUserLoginFromToken("eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJlbGFyYS1sb2dpbiIsImF1ZCI6ImVsYXJhIiwiaWF0IjoxNjgxNzU5NDg4LCJzdWIiOiJhZG1pbkBnbWFpbC5jb20ifQ.UKlDVn-prdw7RDQDwFg0DwaBW2OKU9eJtphsoBRrqZM1EaEYPqb_5YVxBAXoyPYuEcTdlzoUfIYH2kHqa3Uw5Q"));
+        };
+    }
 }
