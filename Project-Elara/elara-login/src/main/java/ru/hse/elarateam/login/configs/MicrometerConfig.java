97d346e60d2c89ab875778da0c8714d6d67c156d
germandilio <glmikhaylov@edu.hse.ru>
2023-04-17 20:44:00 +0300

    Minor fixes

--- Project-Elara/elara-login/src/main/java/ru/hse/elarateam/login/configs/MicrometerConfig.java
+++ Project-Elara/elara-login/src/main/java/ru/hse/elarateam/login/configs/MicrometerConfig.java
@@ -3,6 +3,8 @@ package ru.hse.elarateam.login.configs;
 import feign.Capability;
 import feign.micrometer.MicrometerCapability;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.prometheus.client.exemplars.tracer.otel.OpenTelemetrySpanContextSupplier;
+import io.prometheus.client.exemplars.tracer.otel_agent.OpenTelemetryAgentSpanContextSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
