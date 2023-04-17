82f12e64965a51d03b89bc170afbbcf413715fad
lemon-doge <ddtatyanchenko@edu.hse.ru>
2023-04-17 13:19:19 +0300

    admin console jwt token verification added

--- Project-Elara/elara-admin-console/src/main/java/ru/hse/elarateam/adminconsole/auth/loginservice/LoginServiceFeignClient.java
+++ Project-Elara/elara-admin-console/src/main/java/ru/hse/elarateam/adminconsole/auth/loginservice/LoginServiceFeignClient.java
@@ -1,7 +1,6 @@
 package ru.hse.elarateam.adminconsole.auth.loginservice;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
